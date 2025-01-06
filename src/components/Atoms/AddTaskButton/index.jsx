b9d5a6b9e9d74a89610604c5e53af67369340069
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-01-06 23:35:49 +0900

    AddTaskButtonのデザイン完成

--- src/components/Atoms/AddTaskButton/index.jsx
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -1,10 +1,31 @@
-import React from "react";
-import plus from "../../../assets/svg/plus"
+import React, { useState } from "react";
+import plus from "../../../assets/svg/plus.svg"
 
 export const Img = () => {
-    return <img src={plus} alt="" />;
+    return <img src={plus} style={{
+        width: 20,
+        height: 20
+    }} />;
 }
 
 export const AddTaskButton = (props) => {
-    return <button onClick={props.onClick}><Img></Img>タスクを追加</button>;
+    const [isHovered, setIsHovered] = useState(false);
+    return <button 
+    onClick={props.onClick}
+    onMouseEnter={() => setIsHovered(true)}
+    onMouseLeave={() => setIsHovered(false)}
+    style={{
+        padding: "2px 6px",
+        width: 126,
+        height: 24,
+        borderRadius: 12,
+        display: "flex",
+        alignItems: "center",
+        border: "none",
+        gap: 10,
+        backgroundColor: isHovered ? "rgba(70, 163, 129, 0.2)" : "rgba(255, 0, 0, 0)"
+    }}
+    ><Img></Img>
+    <span style={{color: '#46A381'}}>タスクを追加</span>
+    </button>;
 }
---
+++ src/components/Atoms/AddTaskButton/index.stories.jsx
@@ -0,0 +1,8 @@
+import { AddTaskButton } from "./index";
+
+export default { component: AddTaskButton};
+export const Defult = {
+    args: {
+        onclick: () => console.log("clicked")
+    },
+};
