d34cedb138dbf644c8547f49027161cf851154ae
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-01-06 17:39:41 +0900

    EditButton作成

---
+++ src/components/Atoms/EditButton/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import pencil from "../../../assets/svg/pencil.svg";
+
+export const Img = () => {
+  return <img src={pencil}/>;
+}
+
+export const SampleButton = (props) => {
+    return <button onClick={props.onClick}><Img></Img>{props.label}</button>;
+  }
+  
---
+++ src/components/Atoms/EditButton/index.stories.jsx
@@ -0,0 +1,8 @@
+import { SampleButton } from "./index";
+
+export default { component: SampleButton};
+export const Default = {
+    args: {
+      onClick: () => console.log("clicked"),
+    },
+  };
