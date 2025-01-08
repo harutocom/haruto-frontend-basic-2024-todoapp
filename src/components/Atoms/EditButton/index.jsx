d8249ced5572cf4e0be4f6d7288b73f83188fdfd
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-01-08 17:15:57 +0900

    EditButtonの修正

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import pencil from "../../../assets/svg/pencil.svg";
+import "./EditButton.css"
 
 export const Img = () => {
-  return <img src={pencil}/>;
+  return <img src={pencil}
+  width={20}
+  height={20}
+  />;
 }
 
 export const SampleButton = (props) => {
-    return <button onClick={props.onClick}><Img></Img>{props.label}</button>;
+    return <button
+    onClick={props.onClick}
+    className="EditButton"
+    ><Img></Img>{props.label}</button>;
 }
   
