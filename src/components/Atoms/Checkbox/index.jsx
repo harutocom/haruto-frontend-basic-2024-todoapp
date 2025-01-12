22a10fa47296128a505963f5286c836ff8f9ae21
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-01-12 18:20:12 +0900

    Checkboxのスタイルの記述方法を変更

--- src/components/Atoms/Checkbox/index.jsx
+++ src/components/Atoms/Checkbox/index.jsx
@@ -1,9 +1,26 @@
-import React from "react";
-import  "./Checkbox.css"
+import React, { useState } from "react";
+import check from "../../../assets/svg/check.svg"
+
+// export const Img = () => {
+//     return <img src={check}></img>;
+// }
 
 export const Checkbox = (props) => {
+    const [isHovered, setIsHovered] = useState(false);
     return <button
     onClick={props.onClick}
-    className="checkbox"
+    onMouseEnter={() => setIsHovered(true)}
+    onMouseLeave={() => setIsHovered(false)}
+    style={{
+        width: 20,
+        height: 20,
+        backgroundColor: "transparent",
+        border: "2px solid #B8B8B8",
+        borderRadius: 2,
+        backgroundImage: isHovered
+        ? `url(${check})`
+        : "none",
+        backgroundSize: "cover"
+    }}
     ></button>
 }
