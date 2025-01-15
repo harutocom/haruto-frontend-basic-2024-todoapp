da519c3b4cc314e9e6d580df7b945af51d75f796
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-01-15 14:01:15 +0900

    EditButton修正

--- src/components/Atoms/EditButton/index.jsx
+++ src/components/Atoms/EditButton/index.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import pencil from "../../../assets/svg/pencil.svg";
 
 export const Img = () => {
-  return <img src={pencil}/>;
+  return <img src={pencil}
+  width={20}
+  height={20}
+  />;
 }
 
 export const SampleButton = (props) => {
-    return <button onClick={props.onClick}><Img></Img>{props.label}</button>;
-  }
-  
+  const [isHovered, setIsHovered] = useState(false);
+    return <button
+    onClick={props.onClick}
+    onMouseEnter={() => setIsHovered(true)}
+    onMouseLeave={() => setIsHovered(false)}
+    className="EditButton"
+    style={{
+      padding: 0,
+      width: 20,
+      height: 20,
+      backgroundColor: isHovered
+      ? "rgba(184,184,184,0.2)"
+      : "transparent",
+      border: "none",
+      borderRadius: 50,
+      position: "relative"
+    }}
+    ><Img></Img>{props.label}</button>;
+}
