a4c73f6e3c376f93b32667061a9a7d5d8e6246d3
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-03-06 08:16:12 +0900

    Input/index.stories.jsxの修正

--- src/components/Atoms/Input/index.stories.jsx
+++ src/components/Atoms/Input/index.stories.jsx
@@ -3,6 +3,7 @@ import  Input  from "./index";
 export default { component: Input};
 export const Default = {
     args: {
-      onClick: () => console.log("clicked"),
+      onEditComplete: () => {},
+      defaultValue: '',
     },
   };
