333d87c0bfc45fe4f4e91c858172d18dbf63cee9
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-03-05 10:32:36 +0900

    Enterを押したときにonEditComplete実行

--- src/components/Atoms/Input/index.jsx
+++ src/components/Atoms/Input/index.jsx
@@ -4,12 +4,25 @@ import COLOR from "../../../variables/color";
 
 const Input = ({ onEditComplete, defaultValue }) => {
     const InputRef = useRef();
+    const handleBlur = () => {
+        onEditComplete(InputRef.current.value);
+    }
+
+    const handleKeyDown = (event) => {
+        if(event.key === 'Enter') {
+            onEditComplete(InputRef.current.value);
+        }
+    }
 
     useEffect(() => {
         InputRef.current.focus();
     },[])
 
-  return <StyledInput defaultValue={defaultValue || ''} ref={InputRef}/>
+  return <StyledInput 
+    defaultValue={defaultValue || ''} 
+    ref={InputRef} 
+    onBlur={handleBlur}
+    onKeyDown={handleKeyDown}/>
 }
 
 const StyledInput = styled.input`
