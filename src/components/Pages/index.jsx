c08bba9d11c95a66a8a7e2da73673ff10c48bf39
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-03-29 17:17:22 +0900

    スタイル修正

--- src/components/Pages/index.jsx
+++ src/components/Pages/index.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
-import TodoCard from '../Organisms/TodoCard'
-import { Title } from '../Atoms/Title'
-import styled from 'styled-components'
+import React from "react";
+import TodoCard from "../Organisms/TodoCard";
+import { Title } from "../Atoms/Title";
+import styled from "styled-components";
 
 const index = () => {
   return (
-    <>
-        <StyledTitleWrapper>
-            <Title/>
-        </StyledTitleWrapper>
-        <TodoCard/>
-    </>
+    <StyledWrapper>
+      <StyledTitleWrapper>
+        <Title />
+      </StyledTitleWrapper>
+      <StyledTodoCardWrapper>
+        <TodoCard />
+      </StyledTodoCardWrapper>
+    </StyledWrapper>
+  );
+};
 
-  )
-}
+const StyledWrapper = styled.div`
+  padding: 60px 20px 20px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
 
 const StyledTitleWrapper = styled.div`
-text-align: center;
-`
+  text-align: center;
+`;
 
-export default index
+const StyledTodoCardWrapper = styled.div`
+  width: 100%;
+  max-width: 500px;
+`;
+
+export default index;
