0006a7a1b48ebb07f5cbe88f90f4aeba9e2d8a3b
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-03-29 11:39:30 +0900

    コンポーネントを配置

--- src/components/Organisms/TodoCard/index.jsx
+++ src/components/Organisms/TodoCard/index.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import {React, useState} from 'react'
+import styled from 'styled-components';
+import AddTaskButton from '../../Atoms/AddTaskButton/index';
+import Task from '../../Molecules/Task';
 
 const TodoCard = () => {
+    const [taskList, setTaskList] = useState([]);
   return (
-    <div>TodoCard</div>
+    <StyledWrapper>
+        <AddTaskButton>
+            <StyledTaskList>
+                {taskList.map((task,index) =>{
+                    <Task />
+                })}
+            </StyledTaskList>
+        </AddTaskButton>
+    </StyledWrapper>
   )
 }
 
-export default TodoCard
+const StyledWrapper = styled.div`
+    
+`
+
+const StyledTaskList = styled.div`
+    
+`
+
+export default TodoCard;
