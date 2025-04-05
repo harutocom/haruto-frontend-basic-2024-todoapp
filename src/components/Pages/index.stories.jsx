4fd16ffc08905e15b0d106ea669a884d09a16428
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-04-05 12:42:36 +0900

    commit

--- src/components/Pages/index.stories.jsx
+++ src/components/Pages/index.stories.jsx
@@ -1,6 +1,20 @@
+import React from "react";
 import MainPage from "./index";
+import { AlertHandlerProvider } from "../../contexts/alert_handler";
+import { AlertManager } from "../Organisms/AlertManager/index";
 
-export default { component: MainPage};
-export const Default = {
-    args: {}
-};
+export default { component: MainPage };
+const Template = (args) => <MainPage {...args} />;
+export const Default = Template.bind({});
+Default.args = {};
+
+Default.decorators = [
+  (Story) => (
+    <div>
+      <AlertHandlerProvider>
+        <AlertManager />
+        <Story />
+      </AlertHandlerProvider>
+    </div>
+  ),
+];
