4cc67ee241cbc0143e7f0a5896a3a8e61fde5de1
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-04-05 13:09:13 +0900

    修正

--- src/components/Atoms/Alert/index.stories.jsx
+++ src/components/Atoms/Alert/index.stories.jsx
@@ -1,6 +1,18 @@
+import React from "react";
+import { AlertHandlerProvider } from "../../../contexts/alert_handler";
 import Alert from "./index";
 
 export default { component: Alert };
 export const Default = {
   args: {},
 };
+
+Default.decorators = [
+  (Story) => (
+    <div>
+      <AlertHandlerProvider>
+        <Story />
+      </AlertHandlerProvider>
+    </div>
+  ),
+];
